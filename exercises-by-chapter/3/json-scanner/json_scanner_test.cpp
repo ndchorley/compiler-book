adbbd4e9dacd195894ee37c0c6eae2ea6533c2f4
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-10-10 14:15:02 +0100

    Throw if the string doesn't start with double quotes

--- exercises-by-chapter/3/json-scanner/json_scanner_test.cpp
+++ exercises-by-chapter/3/json-scanner/json_scanner_test.cpp
@@ -11,10 +11,13 @@ enum class Token {
     STRING
 };
 
+class InvalidJson : std::exception {};
+
 vector<Token> scan(string json) {
+    if (json.front() != '"') throw InvalidJson();
+
     return vector { Token::STRING };
 }
-
 
 TEST(JsonScannerTest, It_finds_a_string) {
     auto json = R"("cat")"s;
@@ -22,4 +25,10 @@ TEST(JsonScannerTest, It_finds_a_string) {
     auto result = scan(json);
 
     ASSERT_THAT(result.front(), testing::Eq(Token::STRING));
+}
+
+TEST(JsonScannerTest, It_fails_if_the_string_does_not_start_with_quotes) {
+    auto invalid_json = R"(cat")"s;
+
+    ASSERT_THROW(scan(invalid_json), InvalidJson);
 }
